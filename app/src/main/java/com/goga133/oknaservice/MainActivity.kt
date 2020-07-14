7af7245384cbe7d964bdbe9381130096492cae7a
Андрей Романюк <asromanyuk@edu.hse.ru>
2020-07-15 00:34:13 +0300

    Обновление цветовой гаммы

--- app/src/main/java/com/goga133/oknaservice/MainActivity.kt
+++ app/src/main/java/com/goga133/oknaservice/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 
 
